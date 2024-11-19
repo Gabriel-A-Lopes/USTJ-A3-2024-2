e9e2abee2fafbdfa725f298ddd31817bed9eae7d
Alan Binato <alan.binato@gmail.com>
2024-11-19 13:25:15 -0300

    Updated SQL Script

--- src/main/java/database/dbCoracaoPet.sql
+++ src/main/java/database/dbCoracaoPet.sql
@@ -2,18 +2,30 @@ drop database dbCoracaoPet;
 create database dbCoracaoPet;
 use dbCoracaoPet;
 create table tb_funcionarios(
-	codigoFunc int primary key auto_increment,
+	idFuncionario int primary key auto_increment,
     nomeFunc varchar(200),
     org varchar(200),
     loginFunc varchar(200),
     senhaFunc varchar(200)
 );
 create table tb_admin(
-	codigoADM int primary key auto_increment,
+	idADM int primary key auto_increment,
     nomeADM varchar(200),
     loginADM varchar(200),
     senhaADM varchar(200)
 );
+
+create table tb_animais(
+	idAnimal int primary key auto_increment,
+    nomeAnimal varchar(200),
+    tipoAnimal varchar(200),
+    racaAnimal varchar(200),
+    cor varchar(200),
+    local varchar(200),
+    idade int,
+    castrado boolean
+);
+
 insert into tb_funcionarios
 (nomeFunc, org, loginFunc, senhaFunc) values
 ("Alan", "Coração Pet", "alan.binato", "123asdfg");
@@ -22,5 +34,10 @@ insert into tb_admin
 (nomeADM, loginADM, senhaADM) values
 ("Administrador", "adm", "ADMAccess");
 
+
 select * from tb_funcionarios;
 select * from tb_admin;
+select * from tb_animais;
+
+delete from tb_funcionarios where codigoFunc = 4;
+delete from tb_animais;
